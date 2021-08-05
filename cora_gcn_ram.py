c4fc1207cc672cab7e1e7f775cef336dc45d2876
raymonddyu <yyujwww@163.com>
2021-08-06 00:56:08 +0800

    moka

--- cora_gcn_ram.py
+++ cora_gcn_ram.py
@@ -19,7 +19,7 @@ for _ in range(times):
     # from pmem
     path = "/mnt/mem/project_moka/data/Cora/"
     #path = "/mnt/tmpfs/project_moka/data/Cora/"
-    #path = "/mnt/ramdisk/project_moka/data/Cora/"
+    path = "/mnt/ramdisk/project_moka/data/Cora/"
     # form ram
     #path = "data/Cora/"
     cites = path + "cora.cites"
