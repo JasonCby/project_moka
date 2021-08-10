ca4f3fdfb00a74adbe711e5d41e5c06bdaac226a
raymonddyu <yyujwww@163.com>
2021-08-10 08:54:56 +0800

    dataloader

--- reddit.py
+++ reddit.py
@@ -8,8 +8,8 @@ from torch_geometric.nn import GCNConv, SAGEConv, GATConv
 
 path = "/mnt/mem/project_moka/pubmed/"
 #path = "/mnt/ramfs/project_moka/pubmed/"
-#path = "/mnt/ext4ramdisk/project_moka/pubmed/"
-path = "./pubmed/"
+path = "/mnt/ext4ramdisk/project_moka/pubmed/"
+#path = "./pubmed/"
 
 path_Cora = "/mnt/mem/project_moka/data/Cora/"
 # path_Cora = "/mnt/ramfs/project_moka/data/Cora/"
