c1ba5b0c56637d88d71365c9510cf3966dd31862
raymonddyu <yyujwww@163.com>
2021-08-10 08:23:34 +0800

    dataloader

--- t.py
+++ t.py
@@ -9,7 +9,7 @@ from torch_geometric.nn import GCNConv, SAGEConv, GATConv
 path = "/mnt/mem/project_moka/pubmed/"
 # path = "/mnt/ramfs/project_moka/pubmed/"
 # path = "/mnt/ext4ramdisk/project_moka/pubmed/"
-# path = "./pubmed/"
+path = "./pubmed/"
 
 path_Cora = "/mnt/mem/project_moka/data/Cora/"
 # path_Cora = "/mnt/ramfs/project_moka/data/Cora/"
@@ -23,20 +23,18 @@ batch_size = 128
 epoch_num = 20
 
 # pre-load Planetoid
-#dataset_test = Planetoid(root='./pubmed/', name='Pubmed')
+# dataset_test = Planetoid(root='./pubmed/', name='Pubmed')
 
 # start timer
 start = time.perf_counter()
-#dataset_pubmed = Planetoid(root=path, name='Pubmed')
-dataset_Cora = Planetoid(root=path_Cora, name='Cora')
+dataset_pubmed = Planetoid(root=path, name='Pubmed')
+#dataset_Cora = Planetoid(root=path_Cora, name='Cora')
 
 after = time.perf_counter()
 
-dataset = dataset_Cora
+dataset = dataset_pubmed
 # dataset = dataset_pubmed
 data = dataset[0]  # Get the first graph object.
-
-
 
 file_reading = after - start
 
@@ -90,12 +88,14 @@ class GCNNet(torch.nn.Module):
 
 for _ in range(times):
     train_start = time.perf_counter()
-    
+
     from torch_geometric.data import ClusterData, ClusterLoader, DataLoader
 
     torch.manual_seed(32322)
     cluster_data = ClusterData(data, num_parts=128)  # 1. Create subgraphs.
     train_loader = ClusterLoader(cluster_data, batch_size=batch_size, shuffle=True)  # 2. Stochastic partioning scheme.
+
+    load = time.perf_counter()
 
     print()
     total_num_nodes = 0
@@ -131,9 +131,11 @@ for _ in range(times):
     end = time.perf_counter()
     # output duration
     duration = end - train_start
+    loader_time = load - train_start
 
     print('Reading time: %s Seconds' % file_reading)
     print('Training time: %s Seconds' % duration)
+    print('Loader time: %s Seconds' % loader_time)
 
     model.eval()
     _, pred = model(data).max(dim=1)
