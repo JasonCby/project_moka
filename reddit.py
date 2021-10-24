160f869363c4f98896782baa2c572b8453cf372b
raymonddyu <yyujwww@163.com>
2021-10-24 20:11:48 +0800

    cora

--- reddit.py
+++ reddit.py
@@ -92,7 +92,7 @@ for n in range(times):
 
     # start timer
     start = time.perf_counter()
-    dataset_pubmed = Reddit(root=path_ram)
+    dataset_pubmed = Reddit(root=path_pm)
     # dataset_Cora = Planetoid(root=path_Cora, name='Cora')
     # start timer
     after = time.perf_counter()
