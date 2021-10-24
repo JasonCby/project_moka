439ef2fc89aaf3895b19767193be99cca72a4f3a
raymonddyu <yyujwww@163.com>
2021-10-24 21:08:24 +0800

    cora

--- reddit.py
+++ reddit.py
@@ -92,7 +92,7 @@ for n in range(times):
 
     # start timer
     start = time.perf_counter()
-    dataset_pubmed = ShapeNet(root=path_ram+"ShapeNet/")
+    dataset_pubmed = ShapeNet(root=path_pm+"ShapeNet/")
     # dataset_Cora = Planetoid(root=path_Cora, name='Cora')
     # start timer
     after = time.perf_counter()
